849a85742cd1219a92217ad31a1b146f77bb74c5
chakri8826 <kommuchakri8826@gmail.com>
2025-08-13 14:43:37 +0530

    updated user_frontend

--- user_frontend/src/utils/api.js
+++ user_frontend/src/utils/api.js
@@ -3,3 +3,4 @@ export const API_BASE = (import.meta.env.VITE_API_URL || '').replace(/\/$/, '');
 export const apiUrl = (path) => `${API_BASE}${path}`;
 
 export const apiFetch = (path, options = {}) => fetch(apiUrl(path), options);
+export default apiFetch;
